19fddfaac9ac0fc85449809ea812a38761f61988
Caroline <CarolineJTF@gmail.com>
2022-02-27 10:42:18 +0100

    ajout copyright

--- app.js
+++ app.js
@@ -133,3 +133,16 @@ function animateConfettisFood() {
   function isTweeningConfettisFood(){
     return gsap.isTweening('.containerConfettisFood div');
 }
+
+//afficher année en cours sur le copyright
+
+function displayYear (){
+  let date = new Date();
+  let copyright = document.getElementById('copyright');
+
+  date = date.getFullYear();
+
+  return copyright.innerText = date;
+}
+
+displayYear();
